d057cef8bfc933d07ba5bef610ed8abe0c072726
Francisco Martin <30799842+garciafranciscomartn@users.noreply.github.com>
2026-05-06 17:07:14 -0300

    Update cache version to plan-comida-v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plan-comida-v11';
+const CACHE = 'plan-comida-v12';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
